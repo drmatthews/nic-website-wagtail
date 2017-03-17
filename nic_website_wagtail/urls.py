1325a58384f3858249531faaca0ecae83cbf6578
Daniel Matthews <daniel.r.matthews@kcl.ac.uk>
2017-03-17 15:48:30 +0000

    rss feed and social media icons using font awesome

--- nic_website_wagtail/urls.py
+++ nic_website_wagtail/urls.py
@@ -9,6 +9,8 @@ from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailcore import urls as wagtail_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 
+from nicpages.feed import BlogFeed
+
 urlpatterns = [
     url(r'^django-admin/', include(admin.site.urls)),
 
@@ -16,6 +18,8 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
+
+    url(r'blog-feed/$', BlogFeed(), name='blog-feed'),
 
     url(r'', include(wagtail_urls)),
 ]
---
+++ nicpages/feed.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.syndication.views import Feed
+from django.core.urlresolvers import reverse
+from models import BlogPage
+
+class BlogFeed(Feed):
+    title = "Nikon Imaging Centre news articles"
+    link = "/blog-feed/"
+    description = "Nikon Imaging Centre @ King's news as it is published"
+
+    def items(self):
+        return BlogPage.objects.live().order_by('-date')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.intro
