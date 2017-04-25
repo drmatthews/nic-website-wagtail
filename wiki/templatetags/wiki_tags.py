410298cb7a4001f9d6166047280cd55da01609ba
Daniel Matthews <daniel.r.matthews@kcl.ac.uk>
2017-04-25 14:33:48 +0100

    templatetag for site root

--- wiki/templatetags/wiki_tags.py
+++ wiki/templatetags/wiki_tags.py
@@ -12,14 +12,6 @@ register = template.Library()
 #def get_google_maps_key():
 #    return getattr(settings, 'GOOGLE_MAPS_KEY', "")
 
-
-@register.assignment_tag(takes_context=True)
-def get_site_root(context):
-    # NB this returns a core.Page, not the implementation-specific model used
-    # so object-comparison to self will return false as objects would differ
-    return context['request'].site.root_page
-
-
 def has_menu_children(page):
     return page.get_children().live().in_menu().exists()
 
