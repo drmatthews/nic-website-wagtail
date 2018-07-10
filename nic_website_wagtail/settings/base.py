470565787eea3041999c5ee2b2f16c3eb49405bd
Daniel Matthews <daniel.r.matthews@kcl.ac.uk>
2018-07-10 11:52:41 +0100

    add site maps contrib

--- nic_website_wagtail/settings/base.py
+++ nic_website_wagtail/settings/base.py
@@ -72,6 +72,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
     'mod_wsgi.server',
     'wagtailmenus',
 ]
